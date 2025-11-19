b7bf12650ae96b6e014860276cf20ffaf8418e65
abhinavaby <abhinavaby07@gmail.com>
2025-11-19 21:38:24 +0530

    Delete PythonProject17 directory

--- PythonProject17/addition using recursion.py
+++
@@ -1,8 +0,0 @@
-#addition of two numbers using recursion
-def add(a,b):
-    if b==0:
-        return a
-    else:
-        return add(a+1,b-1)
-print(add(1,5))
-
--- PythonProject17/factorial using recursion.py
+++
@@ -1,9 +0,0 @@
-#factorial
-def factorial(n):
-    if n==0:
-        return 1
-    else:
-        return(n*factorial(n-1))
-n=int(input("enter the number"))
-a=factorial(n)
-print(a)
